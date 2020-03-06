a72aa6e17e2c555c05ef14e4dcff4744e75b3cb1
Moses Aaron Samuel <moses@Mosess-MacBook-Pro.local>
2020-03-05 19:39:00 -0800

    add compose and curry utils; refactor module to IIFE

--- util.js
+++ util.js
@@ -1,17 +1,35 @@
-function create(element, attributes) {
-  const el = document.createElement(element);
-  Object.entries(attributes).forEach(function applyAtributes([
-    attrKey,
-    attrVal
-  ]) {
-    el[attrKey] = attrVal;
-  });
+const Util = (function() {
+  function create(element, attributes) {
+    const el = document.createElement(element);
+    Object.entries(attributes).forEach(function applyAtributes([
+      attrKey,
+      attrVal
+    ]) {
+      el[attrKey] = attrVal;
+    });
 
-  return el;
-}
+    return el;
+  }
 
-function append(to = window.document, el) {
-  return to.appendChild(el);
-}
+  function append(to, el) {
+    return to.append(el);
+  }
 
-export { create, append };
+  function compose(...fns) {
+    return function(initialValue) {
+      return fns.reduceRight((acc, fn) => {
+        return fn(acc);
+      }, initialValue);
+    };
+  }
+
+  function curry(fn, seen = []) {
+    return function(...args) {
+      return fn.length === seen.length + args.length
+        ? fn(...seen, ...args)
+        : curry(fn, [...seen, ...args]);
+    };
+  }
+
+  return { create: curry(create), append: curry(append), compose, curry };
+})();
