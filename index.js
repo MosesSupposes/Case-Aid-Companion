2106fd379bfcc69bf10708b66a06a6a4930fc8e5
Moses Aaron Samuel <moses@Mosess-MacBook-Pro.local>
2020-03-05 19:39:13 -0800

    append a destination input to the document

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 /**
- * Imports
- */
-import * as Util from "./util.js";
-
-/**
  * Main
  */
+
+Util.append(document.body, createDestinationInputs());
 
 /**
  * Helpers
  */
 function createInput(attributes) {
-  Util.create("input", attributes);
+  return Util.create("input", attributes);
 }
 
-function createDestinationInputs(fromAttributes, toAttrubutes) {
-  const from = Util.create("input", { placeholder: "From", ...fromAttributes });
-  const to = Util.create("input", { placeholder: "To", ...toAttrubutes });
+function createDestinationInputs(fromAttributes = {}, toAttrubutes = {}) {
+  const container = Util.create("div", {});
+
+  const from = createInput({ placeholder: "From", ...fromAttributes });
+  const to = createInput({ placeholder: "To", ...toAttrubutes });
+
+  Util.append(container, from);
+  Util.append(container, to);
+
+  return container;
 }
