b37b89c18fecdff4d077f4751343175b46b0e855
Moses A. Samuel <mosamuel98@gmail.com>
2020-03-30 20:35:41 -0700

    setup api call

--- src/Components/DestinationInputs/DestinationInputs.bs.js
+++ src/Components/DestinationInputs/DestinationInputs.bs.js
@@ -6,45 +6,77 @@ var Caml_array = require("bs-platform/lib/js/caml_array.js");
 
 var initialState = {
   visibleInputs: 1,
-  calculating: false
+  calculating: false,
+  lastStartingPoint: "",
+  lastDestination: ""
 };
 
 function reducer(state, action) {
-  if (action !== 0) {
-    return state;
-  } else {
-    return {
-            visibleInputs: state.visibleInputs + 1 | 0,
-            calculating: state.calculating
-          };
+  switch (action) {
+    case /* AddNewInput */0 :
+        return {
+                visibleInputs: state.visibleInputs + 1 | 0,
+                calculating: state.calculating,
+                lastStartingPoint: state.lastStartingPoint,
+                lastDestination: state.lastDestination
+              };
+    case /* RemoveInput */1 :
+        return state;
+    case /* CalculateDistance */2 :
+        return {
+                visibleInputs: state.visibleInputs,
+                calculating: !state.calculating,
+                lastStartingPoint: state.lastStartingPoint,
+                lastDestination: state.lastDestination
+              };
+    
   }
 }
 
-function createInput(dispatch) {
-  return Curry._1(dispatch, /* AddNewInput */0);
+function calculateDistanceThenCreateNewInput(dispatch) {
+  Curry._1(dispatch, /* AddNewInput */0);
+  return Curry._1(dispatch, /* CalculateDistance */2);
 }
 
 function DestinationInputs(Props) {
   var match = React.useReducer(reducer, initialState);
   var dispatch = match[1];
-  return React.createElement("form", undefined, Caml_array.caml_make_vect(match[0].visibleInputs, React.createElement("div", undefined, React.createElement("label", undefined, "From:", React.createElement("input", {
+  var state = match[0];
+  React.useEffect((function () {
+          var allStartingPoints = (document.querySelectorAll(".from"));
+          var allDestinations = (document.querySelectorAll(".to"));
+          var lastStartingPoint = Caml_array.caml_array_get(allStartingPoints, allStartingPoints.length - 1 | 0);
+          var lastDestination = Caml_array.caml_array_get(allDestinations, allDestinations.length - 1 | 0);
+          console.log(lastStartingPoint);
+          console.log(lastDestination);
+          fetch("https://dog.ceo/api/breeds/image/random/3").then((function (response) {
+                      return response.json();
+                    })).then((function (jsonResponse) {
+                    return Promise.resolve(/* () */0);
+                  })).catch((function (_err) {
+                  return Promise.resolve(/* () */0);
+                }));
+          return ;
+        }), [state.calculating]);
+  return React.createElement("form", undefined, Caml_array.caml_make_vect(state.visibleInputs, React.createElement("div", undefined, React.createElement("label", undefined, "From:", React.createElement("input", {
+                              className: "from",
                               type: "text"
                             })), React.createElement("label", undefined, "To:", React.createElement("input", {
+                              className: "to",
                               type: "text"
                             })))), React.createElement("button", {
                   onClick: (function ($$event) {
                       $$event.preventDefault();
-                      return Curry._1(dispatch, /* AddNewInput */0);
+                      Curry._1(dispatch, /* AddNewInput */0);
+                      return Curry._1(dispatch, /* CalculateDistance */2);
                     })
-                }, "Next destination"), React.createElement("button", {
-                  role: "submit"
-                }, "Calculate Distance"));
+                }, "Next destination"));
 }
 
 var make = DestinationInputs;
 
 exports.initialState = initialState;
 exports.reducer = reducer;
-exports.createInput = createInput;
+exports.calculateDistanceThenCreateNewInput = calculateDistanceThenCreateNewInput;
 exports.make = make;
 /* react Not a pure module */
--- src/Components/DestinationInputs/DestinationInputs.re
+++ src/Components/DestinationInputs/DestinationInputs.re
@@ -1,27 +1,92 @@
+[@bs.val] external fetch: string => Js.Promise.t('a) = "fetch";
+
 type state = {
   visibleInputs: int,
   calculating: bool,
+  lastStartingPoint: string,
+  lastDestination: string,
 };
 type action =
   | AddNewInput
   | RemoveInput
   | CalculateDistance;
 
-let initialState = {visibleInputs: 1, calculating: false};
+let initialState = {
+  visibleInputs: 1,
+  calculating: false,
+  lastStartingPoint: "",
+  lastDestination: "",
+};
 
 let reducer = (state, action) => {
   switch (action) {
   | AddNewInput => {...state, visibleInputs: state.visibleInputs + 1}
   | RemoveInput => state
-  | CalculateDistance => state
+  | CalculateDistance => {...state, calculating: !state.calculating}
   };
 };
 
-let createInput = dispatch => dispatch(AddNewInput);
+let calculateDistanceThenCreateNewInput = dispatch => {
+  dispatch(AddNewInput);
+  dispatch(CalculateDistance);
+};
 
 [@react.component]
 let make = () => {
   let (state, dispatch) = React.useReducer(reducer, initialState);
+
+  React.useEffect1(
+    () => {
+      let allStartingPoints: array('a) = [%bs.raw
+        {|
+          document.querySelectorAll(".from")
+        |}
+      ];
+
+      let allDestinations: array('a) = [%bs.raw
+        {|
+          document.querySelectorAll(".to")
+        |}
+      ];
+      let lastStartingPoint = allStartingPoints[Array.length(
+                                                  allStartingPoints,
+                                                )
+                                                - 1];
+      let lastDestination = allDestinations[Array.length(allDestinations) - 1];
+
+      Js.log(lastStartingPoint);
+      Js.log(lastDestination);
+
+      let distanceMatrixBaseUrl = "https://maps.googleapis.com/maps/api/distancematrix/json?";
+      let apiKey = "AIzaSyDYDdl-3dUk1H59jTBcFU9KAd3UesUR9qE";
+      let distanceMatrixFullUrl =
+        distanceMatrixBaseUrl
+        ++ "origins="
+        ++ state.lastStartingPoint
+        ++ "&destinations="
+        ++ state.lastDestination
+        ++ "&units=imperial"
+        ++ "&key="
+        ++ apiKey;
+
+      Js.Promise.(
+        fetch("https://dog.ceo/api/breeds/image/random/3")
+        |> then_(response => response##json())
+        |> then_(jsonResponse => {
+             //  setState(_previousState => LoadedDogs(jsonResponse##message));
+             Js.Promise.resolve()
+           })
+        |> catch(_err => {
+             //  setState(_previousState => ErrorFetchingDogs);
+             Js.Promise.resolve()
+           })
+        |> ignore
+      );
+
+      None;
+    },
+    [|state.calculating|],
+  );
 
   <form>
     {ReasonReact.array(
@@ -30,21 +95,21 @@ let make = () => {
          <div>
            <label>
              {ReasonReact.string("From:")}
-             <input type_="text" />
+             <input type_="text" className="from" />
            </label>
-           <label> {ReasonReact.string("To:")} <input type_="text" /> </label>
+           <label>
+             {ReasonReact.string("To:")}
+             <input type_="text" className="to" />
+           </label>
          </div>,
        ),
      )}
     <button
       onClick={event => {
         ReactEvent.Mouse.preventDefault(event);
-        createInput(dispatch);
+        calculateDistanceThenCreateNewInput(dispatch);
       }}>
       {ReasonReact.string("Next destination")}
-    </button>
-    <button role="submit">
-      {ReasonReact.string("Calculate Distance")}
     </button>
   </form>;
 };
